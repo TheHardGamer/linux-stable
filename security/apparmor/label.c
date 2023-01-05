2c664766beda5911f169a27ac3f5c4ef50c48b7f
TheHardGamer <gamelovr695@gmail.com>
2023-01-05 15:43:59 +0530

    security:apparmor: don't require auditing
    Extends ba5ec92d2e4cb6d4060350ea3f8699d760d06634

--- security/apparmor/label.c
+++ security/apparmor/label.c
@@ -1731,7 +1731,7 @@ int aa_label_acntsxprint(char __counted **strp, struct aa_ns *ns,
 	return aa_label_snxprint(*strp, size + 1, ns, label, flags);
 }
 
-
+#ifdef CONFIG_AUDIT
 void aa_label_xaudit(struct audit_buffer *ab, struct aa_ns *ns,
 		     struct aa_label *label, int flags, gfp_t gfp)
 {
@@ -1761,6 +1761,7 @@ void aa_label_xaudit(struct audit_buffer *ab, struct aa_ns *ns,
 
 	kfree(name);
 }
+#endif
 
 void aa_label_seq_xprint(struct seq_file *f, struct aa_ns *ns,
 			 struct aa_label *label, int flags, gfp_t gfp)
@@ -1809,6 +1810,7 @@ void aa_label_xprintk(struct aa_ns *ns, struct aa_label *label, int flags,
 		pr_info("%s", label->hname);
 }
 
+#ifdef CONFIG_AUDIT
 void aa_label_audit(struct audit_buffer *ab, struct aa_label *label, gfp_t gfp)
 {
 	struct aa_ns *ns = aa_get_current_ns();
@@ -1816,6 +1818,7 @@ void aa_label_audit(struct audit_buffer *ab, struct aa_label *label, gfp_t gfp)
 	aa_label_xaudit(ab, ns, label, FLAG_VIEW_SUBNS, gfp);
 	aa_put_ns(ns);
 }
+#endif
 
 void aa_label_seq_print(struct seq_file *f, struct aa_label *label, gfp_t gfp)
 {
